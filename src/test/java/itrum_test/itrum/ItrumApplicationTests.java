a57081629f7e351ac0fc0be4d9b0ac70bc853ac7
SergeyGT <sergeyburdenyuk@mail.ru>
2026-07-16 12:57:36 +0300

    Init project

---
+++ src/test/java/itrum_test/itrum/ItrumApplicationTests.java
@@ -0,0 +1,13 @@
+package itrum_test.itrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
